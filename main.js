b35e9dd9f2916698bda9771fb871edecd189df47
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-04-20 07:53:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+// go after siteinactive and sitesactive which is on 210 line
+
+// sitesInactive.forEach((s) => {
+//     new g.maps.Marker({
+//       position: s.position,
+//       icon: `https://maps.google.com/mapfiles/kml/paddle/wht-stars-lv.png`,
+//       map: gMap,
+//     });
+//   });
+
+// remove every thing below it  and add the lines which i write below
+
+
+
